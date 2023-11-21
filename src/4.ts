06d4a064105e341816bb1412a2d81daab392bd61
Illia Shatokhin <resonane.2106@gmail.com>
2023-11-21 16:50:25 +0200

    finished 4 task

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,65 @@
-const key = new Key();
+interface IKey {
+  getSignature(): number;
+}
+
+interface IPerson {
+  getKey(): IKey;
+}
+
+interface IHouse {
+  comeIn(person: IPerson): void;
+  openDoor(key: IKey): void;
+}
+
+class Key implements IKey {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person implements IPerson {
+  constructor(private key: IKey) {}
+
+  getKey(): IKey {
+    return this.key;
+  }
+}
+
+abstract class House implements IHouse {
+  protected tenants: IPerson[] = [];
+
+  constructor(protected door: boolean = false, protected key: IKey) {}
+
+  public comeIn(person: IPerson): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: IKey): void;
+}
+
+class MyHouse extends House implements IHouse {
+  constructor(key: IKey) {
+    super(false, key);
+  }
+
+  public openDoor(key: IKey): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key: IKey = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key);
+const person: IPerson = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
